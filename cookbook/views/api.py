9f01750501391c63231204f0c0e5de007418083c
smilerz <smilerz@gmail.com>
2021-03-09 11:29:10 -0600

    import raw json/html

--- cookbook/views/api.py
+++ cookbook/views/api.py
@@ -32,7 +32,8 @@ from cookbook.helper.permission_helper import (CustomIsAdmin, CustomIsGuest,
                                                CustomIsOwner, CustomIsShare,
                                                CustomIsShared, CustomIsUser,
                                                group_required)
-from cookbook.helper.recipe_url_import import get_from_html, find_recipe_json
+from cookbook.helper.recipe_url_import import get_from_html
+from cookbook.helper.recipe_raw_import import get_from_raw
 from cookbook.models import (CookLog, Food, Ingredient, Keyword, MealPlan,
                              MealType, Recipe, RecipeBook, ShoppingList,
                              ShoppingListEntry, ShoppingListRecipe, Step,
@@ -748,6 +749,31 @@ def recipe_from_url(request):
             },
             status=400
         )
+    return get_from_html(response.text, url, request.space)
+
+
+@group_required('user')
+def recipe_from_raw(request):
+    raw_text = request.POST['raw_text']
+    recipe_json, recipe_tree = get_from_raw(raw_text)
+    return JsonResponse({
+        'recipe_tree': recipe_tree,
+        'recipe_json': recipe_json
+    })
+
+
+@group_required('admin')
+def get_backup(request):
+    if not request.user.is_superuser:
+        return HttpResponse('', status=403)
+
+    md_json = json.loads(mjson)
+    for ld_json_item in md_json:
+        # recipes type might be wrapped in @graph type
+        if '@graph' in ld_json_item:
+            for x in md_json['@graph']:
+                if '@type' in x and x['@type'] == 'Recipe':
+                    md_json = x
 
 
 @group_required('admin')
