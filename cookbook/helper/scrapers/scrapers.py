83650abd161567247dd711ce1e4559f8bd32b31d
smilerz <smilerz@gmail.com>
2021-04-02 15:19:20 -0500

    added ability to create custom scrapers

--- cookbook/helper/scrapers/scrapers.py
+++ cookbook/helper/scrapers/scrapers.py
@@ -1,28 +1,25 @@
 from bs4 import BeautifulSoup
-from json import JSONDecodeError
-from recipe_scrapers import SCRAPERS, get_host_name
+from recipe_scrapers import SCRAPERS, get_domain, _exception_handling
 from recipe_scrapers._factory import SchemaScraperFactory
 from recipe_scrapers._schemaorg import SchemaOrg
 
 from .cooksillustrated import CooksIllustrated
 
 CUSTOM_SCRAPERS = {
-    CooksIllustrated.host(site="cooksillustrated"): CooksIllustrated,
-    CooksIllustrated.host(site="americastestkitchen"): CooksIllustrated,
-    CooksIllustrated.host(site="cookscountry"): CooksIllustrated,
+    CooksIllustrated.host(): CooksIllustrated,
 }
-SCRAPERS.update(CUSTOM_SCRAPERS)
 
-
+SCRAPERS = SCRAPERS.update(CUSTOM_SCRAPERS)
+#%%
 def text_scraper(text, url=None):
     domain = None
     if url:
-        domain = get_host_name(url)
+        domain = get_domain(url)
     if domain in SCRAPERS:
         scraper_class = SCRAPERS[domain]
     else:
         scraper_class = SchemaScraperFactory.SchemaScraper
-
+    
     class TextScraper(scraper_class):
         def __init__(
             self,
@@ -30,14 +27,15 @@ def text_scraper(text, url=None):
             url=None
         ):
             self.wild_mode = False
-            # self.exception_handling = None  # TODO add new method here, old one was deprecated
+            self.exception_handling = _exception_handling
             self.meta_http_equiv = False
             self.soup = BeautifulSoup(page_data, "html.parser")
             self.url = url
-            self.recipe = None
             try:
                 self.schema = SchemaOrg(page_data)
             except (JSONDecodeError, AttributeError):
                 pass
 
     return TextScraper(text, url)
+
+# %%
