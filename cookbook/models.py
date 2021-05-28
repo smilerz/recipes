79781b2ea08809956230ecbafde701ce190f07c5
smilerz <smilerz@gmail.com>
2021-05-28 11:37:07 -0500

    split search vectors for recipe name and description

--- cookbook/models.py
+++ cookbook/models.py
@@ -350,6 +350,7 @@ class Step(ExportModelOperationsMixin('step'), models.Model, PermissionModelMixi
     order = models.IntegerField(default=0)
     file = models.ForeignKey('UserFile', on_delete=models.PROTECT, null=True, blank=True)
     show_as_header = models.BooleanField(default=True)
+    search_vector = SearchVectorField(null=True)
 
     objects = ScopedManager(space='recipe__space')
 
@@ -366,6 +367,7 @@ class Step(ExportModelOperationsMixin('step'), models.Model, PermissionModelMixi
 
     class Meta:
         ordering = ['order', 'pk']
+        indexes = (GinIndex(fields=["search_vector"]),)
 
 
 class NutritionInformation(models.Model, PermissionModelMixin):
@@ -417,7 +419,8 @@ class Recipe(models.Model, PermissionModelMixin):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    search_vector = SearchVectorField(null=True)
+    name_search_vector = SearchVectorField(null=True)
+    desc_search_vector = SearchVectorField(null=True)
     space = models.ForeignKey(Space, on_delete=models.CASCADE)
 
     # load custom manager for full text search if postgress is available
@@ -430,7 +433,7 @@ class Recipe(models.Model, PermissionModelMixin):
         return self.name
 
     class Meta():
-        indexes = (GinIndex(fields=["search_vector"]),)
+        indexes = (GinIndex(fields=["name_search_vector", "desc_search_vector"]),)
 
 
 class Comment(ExportModelOperationsMixin('comment'), models.Model, PermissionModelMixin):
