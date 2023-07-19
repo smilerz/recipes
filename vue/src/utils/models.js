cb037e91ed74c545dd7b37cba6463360b5b29cfb
smilerz <smilerz@gmail.com>
2023-07-19 16:56:09 -0500

    fix model.js

--- vue/src/utils/models.js
+++ vue/src/utils/models.js
@@ -781,6 +781,7 @@ export class Models {
                 "updatedon",
                 "viewedon",
                 "makenow",
+                "never_used_food",
                 "page",
                 "pageSize",
                 "options",
